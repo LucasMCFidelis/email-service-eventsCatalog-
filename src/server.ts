8c75d12d43eb745da8312c22c5b37837b188fc81
Lucas Fidelis <lucasm241301@gmail.com>
2025-01-20 14:53:09 -0300

    Adiciona cpy-cli como dependencia de desenvolvimento

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { recoveryCodeRoutes } from "./routes/recoveryCodeRoutes.js";
 const server = Fastify();
 
 // Registrar rotas de usuários com prefixo
-server.register(recoveryCodeRoutes, { prefix: "/email" });
+server.register(recoveryCodeRoutes, { prefix: "/emails" });
 
 // Configurar a porta e host
 const PORT = Number(process.env.PORT) || 3000;
