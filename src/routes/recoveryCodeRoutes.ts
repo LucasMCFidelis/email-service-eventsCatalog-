b877f4e71555deb839885bddc7d7ef946df425bc
Lucas Fidelis <lucasm241301@gmail.com>
2025-01-20 15:09:52 -0300

    Ajusta caminho da rota de envio de código de recuperação

--- src/routes/recoveryCodeRoutes.ts
+++ src/routes/recoveryCodeRoutes.ts
@@ -2,6 +2,6 @@ import { FastifyInstance } from "fastify";
 import { sendRecoveryCodeRoute, validateRecoveryCodeRoute } from "../controllers/recoveryCodeController.js";
 
 export async function recoveryCodeRoutes(fastify: FastifyInstance) {
-  fastify.post("/recuperacao/enviar-codigo", sendRecoveryCodeRoute);
+  fastify.post("/send-recovery-code", sendRecoveryCodeRoute);
   fastify.post("/validate-recovery-code", validateRecoveryCodeRoute);
 }
