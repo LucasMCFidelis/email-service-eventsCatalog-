7ccbd5cd30e572eb4b602ab938c207c601dc2fb1
Lucas Fidelis <lucasm241301@gmail.com>
2025-04-12 17:25:56 -0300

    Adiciona collection de testes do postman e configura para execução direto no terminal

---
+++ src/tests/run-email-service.js
@@ -0,0 +1,32 @@
+import newman from "newman";
+import fs from "fs";
+
+// Carregando o arquivo JSON usando fs
+const emailCollection = JSON.parse(
+  fs.readFileSync("./src/tests/emailService.postman_collection.json", "utf-8")
+);
+
+newman.run(
+  {
+    collection: emailCollection,
+    reporters: ["cli", "html"],
+    reporter: {
+      html: {
+        export: "./src/tests/reports/emailsTest.html",
+      },
+    },
+    envVar: [
+      {
+        key: "email_service_url",
+        value: "https://email-service-eventscatalog.onrender.com/emails",
+      },
+    ],
+  },
+  function (err) {
+    if (err) {
+      console.error("Erro ao rodar a collection:", err);
+    } else {
+      console.log("Collection executada com sucesso!");
+    }
+  }
+);
